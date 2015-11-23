7af2be812428a8e44bb27af43127be038ff9cecb
Alexandre <alexandre.lombard@live.fr>
2015-11-23 15:10:16 +0100

    Working progress with the servlet

--- src/main/java/com/illustrationfinder/IllustrationFinderServlet.java
+++ src/main/java/com/illustrationfinder/IllustrationFinderServlet.java
@@ -1,10 +1,16 @@
 package com.illustrationfinder;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * IllustrationFinder servlet
  */
 public class IllustrationFinderServlet extends HttpServlet {
-
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
+    }
 }
